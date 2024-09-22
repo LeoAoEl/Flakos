659c9e27641259be3204e4cc22f926626c59b69b
Leo0114 <leonel0614@gmail.com>
2024-09-22 15:10:22 -0600

    Lenis

--- src/constants/lenisSmoothScroll.js
+++ src/constants/lenisSmoothScroll.js
@@ -2,7 +2,12 @@ import "@styles/lenis.css";
 import Lenis from "lenis";
 
 // Script to handle Lenis library settings for smooth scrolling
-const lenis = new Lenis();
+const lenis = new Lenis({
+  duration: 1.5, // Ajuste de duración para un desplazamiento suave
+  easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // Easing personalizado
+  smooth: true, // Habilita el smooth scrolling
+  lerp: 0.1, // Interpolación más suave para un mejor rendimiento
+});
 
 function raf(time) {
   lenis.raf(time);
